e824865cb9fc38c469e075695df36af6b59f0299
root <felipeoliveramantero@gmail.com>
2024-05-12 15:34:06 -0700

    task

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -11,7 +11,6 @@ request.get(url, (error, response, body) => {
     return;
   }
   const filmData = JSON.parse(body).results;
-  const matches = filmData.filter(film => film.characters.includes(characterId)
-  );
+  const matches = filmData.filter(film => film.characters.includes(characterId));
   console.log(matches.length);
 });
