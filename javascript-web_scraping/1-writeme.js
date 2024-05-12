c26967f759a57e20341096e0f125f97c85061a34
root <felipeoliveramantero@gmail.com>
2024-05-12 14:37:49 -0700

    task

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const filepath = process.argv[2];
+const writedata = process.argv[3];
+
+function filewriter (filepath, writedata) {
+  fs.writeFile(filepath, writedata, 'utf-8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(data);
+    }
+  });
+}
+
+filewriter(filepath, writedata);
