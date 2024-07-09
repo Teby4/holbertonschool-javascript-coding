6785d2f1d901672b423871193a1334bb4e617826
root <felipeoliveramantero@gmail.com>
2024-07-08 17:31:18 -0700

    task

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,18 +1,20 @@
 #!/usr/bin/node
 
-const readline = require('readline').createInterface({
+const readline = require('readline')
+
+const line = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
 console.log("Welcome to Holberton School, what is your name?");
 
-readline.question('', (name) => {
+line.question('', (name) => {
   console.log(`Your name is: ${name}`);
-  readline.close();
+  line.close();
 });
 
-readline.on('close', () => {
+line.on('close', () => {
   console.log('This important software is now closing');
   process.exit(0);
 });
