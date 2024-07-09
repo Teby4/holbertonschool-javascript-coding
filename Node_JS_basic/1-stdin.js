8135ef1efde7e7b616a43b511b5fbf889ebf1381
root <felipeoliveramantero@gmail.com>
2024-07-08 17:32:19 -0700

    task

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,20 +1,18 @@
 #!/usr/bin/node
 
-const readline = require('readline')
-
-const line = readline.createInterface({
+const readline = require('readline').createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
 console.log("Welcome to Holberton School, what is your name?");
 
-line.question('', (name) => {
+readline.question('', (name) => {
   console.log(`Your name is: ${name}`);
-  line.close();
+  readline.close();  // This should be inside the callback
 });
 
-line.on('close', () => {
+readline.on('close', () => {
   console.log('This important software is now closing');
   process.exit(0);
 });
